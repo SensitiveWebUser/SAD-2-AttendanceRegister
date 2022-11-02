e8eb7c3557ca77815b8abfa8838aef2502f24681
ALICE CHAMBERS <alicee5cha@gmail.com>
2022-11-02 18:13:29 +0000

    added neccesary files for PWA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import i18n from './i18n';
-import App from './App';
 import { I18nextProvider } from 'react-i18next';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import App from './App';
+import i18n from './i18n';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import darkTheme from './utils/themes/Dark';
 
 ReactDOM.render(
@@ -18,6 +19,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
