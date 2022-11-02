5d408f27d263bccb3648569068fdc737fdc32e78
Adam <adam@Adams-MacBook-Air.local>
2022-11-02 22:02:09 +0000

    Added Login with capital L

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,18 @@
+export const Login = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      LOGIN TEST PAGE DOES STUFF FOR LOGIN
+      {/* sign up form */}
+      {/* checks the info against restrictions */}
+      {/* creates a new user on submit */}
+      <form>
+        <div></div>
+      </form>
+    </div>
+  );
+};
