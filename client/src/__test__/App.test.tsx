a816092d187299fec9100223961c47ed168ae151
Mightylordx22 <adam@mightylordx.uk>
2022-11-15 15:45:56 +0000

    Implement React Router
    Added NotFound page and test for it
    Moved App to it's own component

--- client/src/__test__/App.test.tsx
+++ client/src/__test__/App.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
-import App from '../App';
+import { App } from '../components/App';
 import i18n from '../i18n';
 
 it('renders without crashing', () => {
---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,31 @@
+import { Login } from '@mui/icons-material';
+import { LinearProgress } from '@mui/material';
+import { Fragment, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+
+import { Home } from '../../pages';
+import { NotFound } from '../../pages/NotFound';
+
+export const App = (): JSX.Element => (
+  <Suspense
+    fallback={
+      <Fragment>
+        <LinearProgress /> <h1>Loading...</h1>
+      </Fragment>
+    }
+  >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Outlet />
+    </Router>
+  </Suspense>
+);
