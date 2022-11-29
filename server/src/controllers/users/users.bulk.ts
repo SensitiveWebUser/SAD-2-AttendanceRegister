43ca51169a4075cbd033880ec7f72ee77d15fcbd
Nathan Hall <nathan@whatshark.com>
2022-11-29 13:01:24 +0000

    🧦 CSV add user passwords

--- server/src/controllers/users/users.bulk.ts
+++ server/src/controllers/users/users.bulk.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import debug from 'debug';
 import { parse } from 'csv-parse';
 import fs from 'fs';
@@ -10,12 +11,44 @@ const connectionId = process.env.AUTH0_DATABASE_IDENTIFIER;
 
 type TypeCSV = {
   email: string;
+  password: string;
+  role: string;
 };
 
 type AuthUsers = {
   email: string;
   email_verified: boolean;
+  custom_password_hash: {
+    algorithm: string;
+    hash: {
+      value: string;
+    };
+  };
 };
+
+async function getAuth0FormatAsync(records: TypeCSV[]): Promise<AuthUsers[]> {
+  const saltRounds = 10;
+
+  // salt passwords using bcrypt and return list of users with salted passwords
+  const users = await Promise.all(
+    records.map(async (record) => {
+      const hash = await bcrypt.hash(record.password, saltRounds);
+
+      return {
+        email: record.email,
+        email_verified: true,
+        custom_password_hash: {
+          algorithm: 'bcrypt',
+          hash: {
+            value: hash,
+          },
+        },
+      };
+    })
+  );
+
+  return users;
+}
 
 export default async function bulkImportAsync(req: Request, res: Response) {
   const file = req.file;
@@ -37,9 +70,7 @@ export default async function bulkImportAsync(req: Request, res: Response) {
     }
 
     // convert CSV into format Auth0 can understand
-    const users: AuthUsers[] = records.map((record) => {
-      return { email: record.email, email_verified: true };
-    });
+    const users = await getAuth0FormatAsync(records);
     const jsonUsers = JSON.stringify(users);
 
     // send bulk import job command to auth0
@@ -48,7 +79,9 @@ export default async function bulkImportAsync(req: Request, res: Response) {
       connection_id: connectionId || '',
       users_json: jsonUsers,
     });
-    logger('sent import job to auth0 successfully');
+    logger(
+      'sent import job to auth0 successfully...this will take time to populate'
+    );
 
     return res.status(200).json(result);
   });
