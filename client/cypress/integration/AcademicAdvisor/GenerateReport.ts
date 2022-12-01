dcf6f70fcb7c051eb808ad0534b83827bb165b4e
ALICE CHAMBERS <alicee5cha@gmail.com>
2022-12-01 10:55:54 +0000

    academic ad test

--- client/cypress/integration/AcademicAdvisor/GenerateReport.ts
+++ client/cypress/integration/AcademicAdvisor/GenerateReport.ts
@@ -7,7 +7,10 @@
 
 describe('AcademicAdvisor', () => {
   it('As an AcademicAdvisor, I would like to be able to generate an attendance report for all students who i am advising', () => {
-    expect(true).to.equal(true);
-    cy.visit('/');
+    cy.get('[data-testid="MenuIcon"]').click();
+    cy.get('[data-testid="reportButton"]').click();
+    cy.get('#courseID').type('643e6d09-275e-4bf9-8566-4910f5994413');
+    cy.get('[data-testid="download"]').click();
+    cy.get('#notistack-snackbar').should('have.text', 'Success!');
   });
 });
--- client/cypress/integration/Admin/ViewAllUsers.ts
+++ client/cypress/integration/Admin/ViewAllUsers.ts
@@ -9,6 +9,11 @@ describe('Admin', () => {
   it('As an Admin, I would like to be able to view a list of all the users in the system', () => {
     cy.get('[data-testid="MenuIcon"]').click();
     cy.get('[data-testid="adminButton"]').click();
-    cy.get('[data-testid="userTableHead"]');
+    cy.get('[data-testid="userTableHead"]').then(() => {
+      cy.get('[data-testid="row-academic-advisor4@localhost.com"]').should(
+        'contain',
+        'academic-advisor4@localhost.com'
+      );
+    });
   });
 });
