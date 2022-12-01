1311ba7b9cf205ed32aceb5e53789cd7a6842ab3
ALICE CHAMBERS <alicee5cha@gmail.com>
2022-12-01 03:30:35 +0000

    text report for student

--- client/src/components/TextReport/TextReport.tsx
+++ client/src/components/TextReport/TextReport.tsx
@@ -15,8 +15,11 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import EditIcon from '@mui/icons-material/Edit';
 import { useAuth0, User } from '@auth0/auth0-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useRequest } from '../../hooks/useRequest';
+import { isEmpty } from 'lodash';
+import { roles } from '../../utils/constants';
 
 const module = ['module1', 'module2', 'module3'];
 const session = ['session1', 'session2', 'session3'];
@@ -24,7 +27,8 @@ const student = ['alice', 'alex', 'adam', 'silvia', 'nathan']; //TODO: get
 const attendance = ['100', '35', '68']; // TODO: replace with user.getAttendance()
 const user = { name: 'alice', type: 'Course Leader' }; //TODO: replace with user
 
-function getEditButton() {
+// a staff member will be able to edit a students attendance
+const getEditButton = () => {
   if (user.type !== 'Student') {
     return (
       <TableCell align="right" sx={{ border: 'none', p: '0px', width: '50px' }}>
@@ -35,9 +39,9 @@ function getEditButton() {
       </TableCell>
     );
   }
-}
+};
 
-function getStudents() {
+const getStudents = () => {
   if (user.type !== 'Student') {
     return student.map((row, index) => (
       <TableRow key={index}>
@@ -50,9 +54,9 @@ function getStudents() {
       </TableRow>
     ));
   }
-}
+};
 
-function ModuleRow(props) {
+const ModuleRow = ({ moduleName }) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -67,8 +71,8 @@ function ModuleRow(props) {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell>{module[props.moduleKey]}</TableCell>
-        <TableCell>{attendance[props.moduleKey]}</TableCell>
+        <TableCell>{moduleName}</TableCell>
+        <TableCell>{'attendance'}</TableCell>
         <TableCell align="right" sx={{ p: '20px' }} />
       </TableRow>
       <TableRow>
@@ -88,17 +92,17 @@ function ModuleRow(props) {
       </TableRow>
     </>
   );
-}
+};
 
 ModuleRow.propTypes = {
-  moduleKey: PropTypes.number,
+  moduleName: PropTypes.string,
 };
 
 interface ModuleRow {
-  moduleKey: number;
+  moduleName: string;
 }
 
-function SessionRow(props) {
+const SessionRow = (props) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -127,7 +131,9 @@ function SessionRow(props) {
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Table size="small" aria-label="modules-sessions-students">
-                <TableBody>{getStudents()}</TableBody>
+                <TableBody>
+                  {getStudents()} {getEditButton()}
+                </TableBody>
               </Table>
             </Box>
           </Collapse>
@@ -135,7 +141,7 @@ function SessionRow(props) {
       </TableRow>
     </>
   );
-}
+};
 
 SessionRow.propTypes = {
   sessionKey: PropTypes.number,
@@ -146,8 +152,77 @@ interface SessionRow {
 }
 
 export const TextReport = (): JSX.Element => {
-  const { user } = useAuth0();
-  return (
+  const { user, isLoading, isAuthenticated } = useAuth0();
+  const [userData, setUserData] = useState([]);
+  const [tutorData, setTutorData] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const role = isAuthenticated
+    ? user['http://sad.assignment.com/userData'].app.role
+    : roles.GUEST;
+
+  // get current users course/module data
+  const [getUserCourses] = useRequest({
+    url: `http://localhost:3001/api/users/${user.sub}/courses`,
+    method: 'get',
+    onSuccess: (data) => {
+      setUserData(data);
+      console.log(data);
+    },
+  });
+
+  // get list of all users
+  const [getUsers] = useRequest({
+    url: ' /api/users',
+    method: 'get',
+    onSuccess: (data) => {
+      setUsers(data);
+      console.log(data);
+    },
+  });
+
+  // get session list for students/module leader
+  //???
+
+  // get session list tutor
+  const [getTutorSessions] = useRequest({
+    url: `http://localhost:3001/api/tutors/${user.sub}/sessions`,
+    method: 'get',
+    onSuccess: (data) => {
+      setTutorData(data);
+      console.log(data);
+    },
+  });
+
+  //userData[0].modules.module_name
+
+  let rowData;
+
+  useEffect(() => {
+    switch (role) {
+    case roles.ADMIN:
+      break;
+    case roles.ACADEMIC_ADVISOR:
+      break;
+    case roles.COURSE_LEADER:
+      break;
+    case roles.MODULE_LEADER:
+      break;
+    case roles.STUDENT:
+      getUserCourses();
+      rowData = userData[0].modules;
+      break;
+    case roles.TUTOR:
+      getTutorSessions();
+      break;
+    default:
+      break;
+    }
+  }, []);
+
+  return isEmpty(userData) ? (
+    <></>
+  ) : (
     <>
       <Typography variant="h1" sx={{ p: '1rem' }}>
         {user.nickname} Attendance
@@ -167,8 +242,8 @@ export const TextReport = (): JSX.Element => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {module.map((row, index) => (
-              <ModuleRow key={index} moduleKey={index} />
+            {rowData.map((row, index) => (
+              <ModuleRow key={index} moduleName={row.module_name} />
             ))}
           </TableBody>
         </Table>
